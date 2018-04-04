03a06e424e2f920f52a8d5b63b54c2c88b89e32f
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2018-04-04 14:15:53 -0700

    added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='bokomaru',
+    version='0.1dev',
+    packages=['bokomaru',],
+    license='MIE',
+    long_description=open('README.rst').read(),
+)
